d6a95dcb2b5c46669d23538a806b0f059fc9da7e
nikkulesh <109527067+nikkulesh@users.noreply.github.com>
2022-09-06 14:11:16 +0300

    Add files via upload
    1.3 Конфигурирование Spring-приложений

---
+++ nkspringboot3/src/main/java/ru/otus/service/QuestionService.java
@@ -0,0 +1,7 @@
+package ru.otus.service;
+
+import java.io.IOException;
+
+public interface QuestionService {
+    void showQuestions();
+}
---
+++ nkspringboot3/src/test/java/ru/otus/service/QuestionServiceImplTest.java
@@ -0,0 +1,50 @@
+package ru.otus.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import ru.otus.service.QuestionServiceImpl;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Сервис по чтению вопросов из scv-файла")
+class QuestionServiceImplTest {
+
+    private QuestionServiceImpl service;
+
+    private final ByteArrayOutputStream consoleOut = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void init() {
+        service = new QuestionServiceImpl();
+        System.setOut(new PrintStream(consoleOut));
+    }
+
+    //не работает
+    /*@Test
+    @DisplayName("сервис должен выводить вопросы в консоль")
+    void QuestionServicePrintCheck() {
+        service.showQuestions();
+        String expectedString = "Question: What's 1+1?" +
+                "\r\n" +
+                "Question: What's 2+2?" +
+                "\r\n" +
+                "Question: What's 3+3?" +
+                "\r\n" +
+                "Question: What's 4+4?" +
+                "\r\n" +
+                "Question: What's 5+5?" +
+                "\r\n";
+        Assertions.assertEquals(expectedString, consoleOut.toString());
+    }*/
+
+}
